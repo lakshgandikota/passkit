11e3d2619f6c88819b8505ea852a2548e6e4dc56
Laks Gandikota <lakshmikaantan@gmail.com>
2012-06-19 22:01:08 -0500

    Updated signing. Thanks to Alex-J

--- get.php
+++ get.php
@@ -15,19 +15,29 @@ ini_set('error_reporting', E_ALL);
 date_default_timezone_set('America/Chicago');
 define('DOCUMENT_ROOT', dirname(realpath(__FILE__)).'/');
 $useragent = $_SERVER['HTTP_USER_AGENT'];
+require_once(DOCUMENT_ROOT . 'settings/globals.php');
+require_once DOCUMENT_ROOT . 'classes/KLogger.php';
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+/*
+//echo $_SERVER['HTTP_USER_AGENT'];
+echo $passname . " > " . filesize($passname);
+die();
+*/
 if (strpos($useragent, "iPhone"))
 {
 	$isIphone = TRUE;
+	
+	header('Content-disposition: attachment; filename=pass.pkpass'); 
+	header("Content-Length: ".filesize($passname)); 
 	header('Content-Type: application/vnd.apple.pkpass');
+	header('Content-Transfer-Encoding: binary'); 
+
 }
 else
 {
 	$isIphone = FALSE;
 }
-require_once(DOCUMENT_ROOT . 'settings/globals.php');
-require_once DOCUMENT_ROOT . 'classes/KLogger.php';
 $log = KLogger::instance(dirname(__FILE__) . '/log/', KLogger::DEBUG);
 $log->logInfo('Start.');
 require_once(DOCUMENT_ROOT . 'classes/manifest.class.php');
@@ -46,7 +56,25 @@ $log->logInfo('End.');
 
 $certdata = openssl_x509_read(file_get_contents($certificate));
 $privkey = openssl_pkey_get_private(file_get_contents($key), $key_password );
-openssl_pkcs7_sign($manifestfile, $signature, $certdata, $privkey,array(),PKCS7_BINARY);
+openssl_pkcs7_sign($manifestfile, $signature . 'tmp', $certdata, $privkey,array(),PKCS7_BINARY|PKCS7_NOATTR|PKCS7_DETACHED);
+
+$signatureRaw = $signature . 'tmp';
+$handle = fopen($signatureRaw, "r");
+$toDecode = fread($handle, filesize($signatureRaw));
+fclose($handle);
+
+
+$pattern = "/.*?Content-Disposition: attachment; filename=\".*?\"(.*?)-----.*?/sm";
+preg_match_all($pattern, $toDecode, $matchResult);
+
+
+$toDecode = base64_decode($matchResult[1][0]);
+
+$fp = fopen($signature, "w");
+fwrite($fp, $toDecode);
+fclose($fp);
+
+unlink($signature . 'tmp');
 
 //Creating pkpass
 
